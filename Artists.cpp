579f3ff9b84e92f76b9f4956d6d062b611c74c82
Derya <130533416+DeriYume@users.noreply.github.com>
2023-06-14 23:46:08 +0300

    Add files via upload

---
+++ Artists.cpp
@@ -0,0 +1,117 @@
+#include "iostream"
+#include "string"
+
+using namespace std;
+
+class artist {
+	string name, country;
+	int songs, age;
+
+	public:
+		artist();
+		void print();
+		void ageOverForty();
+		int get_songs() { return songs; }
+		string get_country() { return country; }
+		int get_age() { return age; }
+};
+
+artist::artist() {
+
+	cout << "Name: ";
+	getline(cin, name);
+
+	cout << "Country: ";
+	getline(cin, country);
+
+	cout << "Age: ";
+	cin >> age;
+
+	cout << "# of songs: ";
+	cin >> songs;
+
+	cin.ignore();
+}
+
+void artist::print() {
+	cout << name << " " << age << " " << country << " " << songs << endl;
+}
+
+void artist::ageOverForty() {
+	if (age > 40) cout << "The artist is older than 40.\n";
+	else cout << "The artist is younger than 40.\n";
+
+}
+
+void youngBulgarianArtists(int size, artist arrtist[]) {
+	for (int i = 1 ; i < size ; i++) {
+		if (arrtist[i].get_country() == "Bulgaria" && arrtist[i].get_age() < 20)
+			arrtist[i].print();
+	}
+}
+
+double averageFrenchArtistAge(int size, artist arrtist[]) {
+	double sum = 0;
+	int count = 0;
+
+	for (int c = 0 ; c < size ; c++) {
+		if (arrtist[c].get_country() == "France") {
+			sum = arrtist[c].get_age() + sum;
+			count++;
+		};
+	};
+
+	return sum / count;
+};
+
+void sort(int size, artist arrtist[])
+{
+	for (int i = 1 ; i < size ; i++)
+		for (int j = 0 ; j <= size - i - 1 ; j++)
+			if (arrtist[j].get_songs() < arrtist[j + 1].get_songs())
+			{
+				artist temp = arrtist[j];
+				arrtist[j] = arrtist[j + 1];
+				arrtist[j + 1] = temp;
+			}
+
+	for (int i = 0 ; i < size ; i++) {
+		arrtist[i].print();
+	}
+}
+
+int main()
+{
+	cout << "Input data about an artist: " << endl;
+	artist firstArtist;
+	firstArtist.print();
+
+	cout << "Input data about another artist: " << endl;
+	artist secondArtist;
+
+	cout << "The artist with more songs is: ";
+	if (firstArtist.get_songs() > secondArtist.get_songs()) firstArtist.print();
+	else secondArtist.print();
+
+	int input;
+
+	do {
+		cout << "Input desired amount of artists: ";
+		cin >> input;
+	} while (input < 2 || input > 50);
+	cin.ignore();
+
+	artist* artistPointer = new artist[input];
+
+	cout << "The average French artist age is " << averageFrenchArtistAge(input, artistPointer) << endl;
+
+	cout << endl;
+	cout << "List of artists from Bulgaria who are younger than 20: " << endl;
+	youngBulgarianArtists(input, artistPointer);
+
+	cout << endl;
+	cout << "Sorting the artists by number of songs: " << endl;
+	sort(input, artistPointer);
+
+	return 0;
+}
